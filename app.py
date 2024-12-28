ebb5ba5a1255b013c884dea50579ae28f82598be
enzhas <kurmashnurzhan@gmail.com>
2024-12-28 11:36:13 +0500

    123123)_-0

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app.config['SESSION_TYPE'] = 'filesystem'
 app.config['SESSION_PERMANENT'] = True  # Optional: Adjust as needed
 app.config['SESSION_COOKIE_SECURE'] = True  # Only send over HTTPS
 app.config['SESSION_COOKIE_HTTPONLY'] = True 
-app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=2)  # Set lifetime to 1 year
+app.config['PERMANENT_SESSION_LIFETIME'] = 3600  # Set lifetime to 1 year
 
 
 @app.before_request
